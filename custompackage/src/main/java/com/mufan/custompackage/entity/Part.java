6ec43ac9f33978891e659ce13f1ebe030a3d3eec
alittlebitcool <617058979@qq.com>
2019-01-16 09:39:32 +0800

    新增part实体类

---
+++ custompackage/src/main/java/com/mufan/custompackage/entity/Part.java
@@ -0,0 +1,28 @@
+package com.mufan.custompackage.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @ Author     ：zyx.
+ * @ Date       ：Created in 9:37 2019/1/16
+ * @ Description：部件实体类
+ * % @author YuXingZh
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "part")
+public class Part {
+    @Id
+    private Integer id;
+    private String partName;
+    private Integer sortId;
+    private Integer imageUrl;
+    private Double partPrice;
+    private Boolean checked;
+}
